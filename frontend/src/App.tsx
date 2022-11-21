595196483aaf26955724012243f5e14bf8f50fdc
diperezlima <dperezlima@gmail.com>
2022-11-20 21:06:29 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,22 @@
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
-  <Header />
-    <main>
-      <section id="sales">
-        <div className="dsmeta-container">
-          <SalesCard />
+      <ToastContainer/>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
 
-        </div>
-      </section>
-    </main>
-  </>
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
